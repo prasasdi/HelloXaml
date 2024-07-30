4c028fdbc9aec1c656fbfe3aa5dcc3c24bb1c783
prasasdi <rizky.adie7@gmail.com>
2024-07-30 22:00:08 +0700

    relaycommand

--- HelloXaml/UIs/UserControlViewModel.cs
+++ HelloXaml/UIs/UserControlViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -16,10 +17,33 @@ namespace HelloXaml.UIs
 
     public class UserControlViewModel
     {
+
         public ObservableCollection<MessageModel> Messages { get; set; }
+
+        #region Commands
+        public RelayCommand DeleteCommand { get; set; }
+        #endregion
+
+        #region To Delete An Entity
+        private MessageModel selectedMessage;
+        public MessageModel SelectedMessage { 
+            get => selectedMessage; 
+            set {
+                selectedMessage = value; 
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
+        public void OnDelete()
+        {
+            //...args to delete dari list of N
+            Messages.Remove(SelectedMessage);
+        }
+        public bool CanDelete() => SelectedMessage != null;
+        #endregion
 
         public UserControlViewModel()
         {
+            DeleteCommand = new RelayCommand(OnDelete, CanDelete);
             Messages = new ObservableCollection<MessageModel>();
             Messages.Add(new MessageModel
             {
